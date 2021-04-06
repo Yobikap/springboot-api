01dcb17dbb71f1364893b2093db0cf861c836ea7
James Ibrahim <j.ibrahim@anaar.nl>
2021-04-06 12:25:13 +0500

    first init

---
+++ src/test/java/nl/ns/international/timezoneexample/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ns.international.timezoneexample.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
